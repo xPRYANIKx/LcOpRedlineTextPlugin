1d2f586112492d97de04de79732be38475cedc2c
Roman Tuleninov <tuleninov.rv@gmail.com>
2024-04-05 15:18:56 +0300

    Adding project files
    Adding project files

---
+++ Main.cs
@@ -0,0 +1,55 @@
+﻿using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Plugins;
+using PRYANIK_Plugin.Services;
+
+
+namespace PRYANIK_Plugin
+{
+    [Plugin("PRYANIK_Plugin", "хPRYANIKх", DisplayName = "PRYANIK_Plugin")]
+    [RibbonLayout("PRYANIK_Plugin.xaml")]
+
+    [RibbonTab("PluginTab", DisplayName = "PRYANIK_Plugin")]
+    [Command("ID_Button_A", DisplayName = "Автоматический комментарий", Icon = "Source\\comment_small.png", LargeIcon = "Source\\comment_big.png", ToolTip = "Создание текстового комментария. Для создания комментария в автоматическом режиме необходимо выделить два эллемента и нажать клавишу Q", CanToggle = true)]
+    public class Main : CommandHandlerPlugin
+    {
+        public override int ExecuteCommand(string name, params string[] parameters)
+        {
+            switch (name)
+            {
+                case "ID_Button_A":
+                    {
+                        if (Application.ActiveDocument.CurrentSelection.SelectedItems.Count == 2)
+                        {
+                            AddComment.GettingDataForAComment();
+                            AddComment.CreateViewpoint();
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            return 0;
+        }
+    }
+}
+
+namespace PRYANIK_Plugin_inputPlugin
+{
+    [Plugin("PRYANIK_inputPlugin", "хPRYANIKх")]
+    public class Main : InputPlugin
+    {
+        public override bool KeyUp(View view, KeyModifiers modifier, ushort key, double timeOffset)
+        {
+            if (Application.ActiveDocument.CurrentSelection.SelectedItems.Count == 2 && key == 81)
+            {
+                AddComment.GettingDataForAComment();
+                AddComment.CreateViewpoint();
+                return true;
+            }
+
+            return base.KeyUp(view, modifier, key, timeOffset);
+        }
+    }
+}
---
+++ Services/AddComment.cs
@@ -0,0 +1,99 @@
+﻿using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Interop;
+using System.Windows.Forms;
+
+namespace PRYANIK_Plugin.Services
+{
+    public static class AddComment
+    {
+        internal static Document doc = Autodesk.Navisworks.Api.Application.ActiveDocument;
+        internal static string textComment;
+
+        internal static void GettingDataForAComment()
+        {
+            Selection storedSelection = doc.CurrentSelection;
+            ModelItem selectElement1 = storedSelection.GetSelectedItems()[0];
+            ModelItem selectElement2 = storedSelection.GetSelectedItems()[1];
+
+            string elementPath(ModelItem item)
+            {
+                string elementFile = item?.Parent?.Parent?.Parent?.Parent?.PropertyCategories.FindPropertyByDisplayName("Элемент", "Файл источника")?.Value.ToString();
+                string elementType = item?.Parent?.Parent?.Parent?.Parent?.Parent?.PropertyCategories.FindPropertyByDisplayName("Элемент", "Тип")?.Value.ToString();
+
+                if (elementType == "DisplayString:Экземпляр" || elementType == "DisplayString:Ссылка")
+                {
+                    return textFilter(item.Parent.Parent.Parent.Parent.Parent.PropertyCategories.FindPropertyByDisplayName("Элемент", "Имя").Value.ToString());
+                }
+                else if (elementFile == null && elementType == null)
+                {
+                    MessageBox.Show("Ошибка при получении имени файла! Проверьте созданный вид.\nПри создании комментария был выбран элемент: {item.DisplayName}");
+                    return "Ошибка-ИМЯ";
+                }
+                else
+                {
+                    return textFilter(elementFile);
+                }
+            };
+
+            string elementID(ModelItem item)
+            {
+                string elementIDValues = item?.PropertyCategories.FindPropertyByDisplayName("ID объекта", "Значение")?.Value.ToString();
+
+                if (elementIDValues == null)
+                {
+                    MessageBox.Show("Ошибка при получении ID элемента! Проверьте созданный вид.\nПри создании комментария был выбран элемент: {item.DisplayName}");
+                    return "Ошибка-ID";
+                }
+                else
+                {
+                    return textFilter(elementIDValues);
+                }
+            };
+
+            string textFilter(string text)
+            {
+                int endIndex = text.IndexOf(".rvt");
+                string cutText = endIndex != -1 ? text.Substring(0, endIndex) : text;
+                return cutText.Replace("DisplayString:", "");
+            }
+
+            textComment = $"{elementPath(selectElement1)}-ID: {elementID(selectElement1)};\n{elementPath(selectElement2)}-ID: {elementID(selectElement2)};";
+        }
+
+        internal static Point2D ScreenToCameraSpace(Point2D pointScreenSpace)
+        {
+            LcOaViewer viewer = doc.ActiveView.Viewer;
+            var pointWindowSpace = LcOpRedline.ScreenToWindowSpace(viewer, pointScreenSpace);
+            var pointCameraSpace = LcOpRedline.WindowToCameraSpace(viewer, pointWindowSpace);
+
+            return pointCameraSpace;
+        }
+
+        internal static void CreateViewpoint()
+        {
+            Viewpoint curentVievpoint = doc.CurrentViewpoint.Value;
+            SavedViewpoint newViewpoint = new SavedViewpoint(curentVievpoint);
+            newViewpoint.DisplayName = "_Вид";
+            doc.SavedViewpoints.AddCopy(newViewpoint);
+
+            var viewpoints = doc.SavedViewpoints;
+            int newIndex = viewpoints.Value.Count - 1;
+            SavedViewpoint viewpointCopy = viewpoints.Value[newIndex].CreateCopy() as SavedViewpoint;
+
+            Point2D point2D = new Point2D(155, 830);
+            var redlineText = new LcOpRedlineText(textComment, ScreenToCameraSpace(point2D));
+            redlineText.SetLineThickness(5);
+            redlineText.SetLineColor(Color.White);
+
+            viewpointCopy.Redlines.Add(redlineText);
+            viewpoints.ReplaceWithCopy(newIndex, viewpointCopy);
+
+            SavedItem item = doc.SavedViewpoints.Value[newIndex];
+            SavedViewpoint savedViewpoint = item as SavedViewpoint;
+            Viewpoint _vievpoint = savedViewpoint.Viewpoint;
+            doc.SavedViewpoints.CurrentSavedViewpoint = item;
+
+            doc.CurrentSelection.Clear();
+        }
+    }
+}
